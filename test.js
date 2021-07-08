00d1c2937201b6da942a564736415a87ddd7d5d6
glorious-min <70811550+glorious-min@users.noreply.github.com>
2021-07-08 23:46:27 +0900

    sssss

--- test.js
+++ test.js
@@ -2,10 +2,10 @@ const arr = (n) => {
   const newArr = [];
   for (let i = 1; i <= 50; i++) {
     if (n % 2 === 0) {
-      newArr.push(n);
+     
     }
   }
   return true;
   ////
-
+sd sd sd sd 
 };
